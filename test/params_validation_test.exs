6b9f0912c6cd3d654e374358f3141496093b5034
Coby Benveniste <coby.b@spot.im>
2018-07-26 16:25:53 +0300

    Merged latest changes from amit to me

--- test/params_validation_test.exs
+++ test/params_validation_test.exs
@@ -13,9 +13,8 @@ defmodule ParamsValidation.Test do
     conn = %{conn | path_params: %{path_param: "lol"}}
 
     assert %{status: nil} =
-             conn
-             |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             conn |> put_private(:params_validator, validator)
+             |> call(log_errors?: false)
   end
 
   test "get with invalid parameters" do
@@ -25,9 +24,8 @@ defmodule ParamsValidation.Test do
     conn = %{conn | path_params: %{path_param: 10}}
 
     assert %{state: :sent, status: 400} =
-             conn
-             |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             conn |> put_private(:params_validator, validator)
+             |> call(log_errors?: false)
   end
 
   test "post with with mixed parameters" do
@@ -42,9 +40,8 @@ defmodule ParamsValidation.Test do
     conn = %{conn | path_params: %{path_param: "10"}}
 
     assert %{state: :unset, body_params: %{body_param: 10, optional_body_param: nil}} =
-             conn
-             |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             conn |> put_private(:params_validator, validator)
+             |> call(log_errors?: false)
   end
 
   test "post with with invalid mixed parameters" do
@@ -59,9 +56,8 @@ defmodule ParamsValidation.Test do
     conn = %{conn | path_params: %{path_param: "10"}}
 
     assert %{state: :sent, status: 400} =
-             conn
-             |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             conn |> put_private(:params_validator, validator)
+             |> call(log_errors?: false)
   end
 
   test "post with with invalid param and optional param" do
@@ -76,9 +72,8 @@ defmodule ParamsValidation.Test do
     conn = %{conn | path_params: %{path_param: "10"}}
 
     assert %{state: :sent, status: 400} =
-             conn
-             |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             conn |> put_private(:params_validator, validator)
+             |> call(log_errors?: false)
   end
 
   test "success when parameter exists" do
@@ -87,7 +82,7 @@ defmodule ParamsValidation.Test do
     assert %{body_params: %{field_1: "hello"}} =
              conn("POST", "/", %{"field_1" => "hello"})
              |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             |> call(log_errors?: false)
   end
 
   test "success when optional not given" do
@@ -97,7 +92,7 @@ defmodule ParamsValidation.Test do
     assert %{state: :unset, body_params: %{field_1: nil}} =
              conn("POST", "/", %{})
              |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             |> call(log_errors?: false)
   end
 
   test "success when optional and not optional given" do
@@ -107,7 +102,7 @@ defmodule ParamsValidation.Test do
     assert %{state: :unset, body_params: %{field_1: nil, field_2: "hello"}} =
              conn("POST", "/", %{"field_2" => "hello"})
              |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             |> call(log_errors?: false)
   end
 
   test "optional given and added to body params" do
@@ -117,7 +112,7 @@ defmodule ParamsValidation.Test do
     assert %{state: :unset, body_params: %{field_1: "field_1"}} =
              conn("POST", "/", %{"field_1" => "field_1"})
              |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             |> call(log_errors?: false)
   end
 
   test "error when parameter missing" do
@@ -126,7 +121,7 @@ defmodule ParamsValidation.Test do
     assert %{state: :sent, status: 400} =
              conn("POST", "/", %{})
              |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             |> call(log_errors?: false)
   end
 
   test "error when parameter type mismatch" do
@@ -135,12 +130,32 @@ defmodule ParamsValidation.Test do
     assert %{state: :sent, status: 400} =
              conn("POST", "/", %{field_1: 1})
              |> put_private(:params_validator, validator)
-             |> params_validation(nil)
+             |> call(log_errors?: false)
   end
 
   test "skip when no validate" do
     assert %{state: :unset, body_params: %{"field_1" => 1}} =
              conn("POST", "/", %{field_1: 1})
-             |> params_validation(nil)
+             |> call(log_errors?: false)
+  end
+
+  test "query params" do
+    [private: %{params_validator: validator}] = expect(query_params: %{field_1: :integer})
+    parser_opts = Plug.Parsers.init(parsers: [:json, :urlencoded], json_decoder: Poison)
+    assert %{state: :unset, query_params: %{field_1: 1}} =
+             conn("POST", "/test?field_1=1", nil)
+             |> put_private(:params_validator, validator)
+             |> Plug.Parsers.call(parser_opts)
+             |> call(log_errors?: false)
+  end
+
+  test "optional query params" do
+    [private: %{params_validator: validator}] = expect(query_params: %{field_1: :integer})
+    parser_opts = Plug.Parsers.init(parsers: [:json, :urlencoded], json_decoder: Poison)
+    assert %{state: :unset, query_params: %{field_1: nil}} =
+             conn("POST", "/test", nil)
+             |> put_private(:params_validator, validator)
+             |> Plug.Parsers.call(parser_opts)
+             |> call(log_errors?: false)
   end
 end
